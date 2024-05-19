57046d7f8f31d2a789b61a02ad9c79f9d568b21e
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-05-18 22:07:38 -0600

    Only connect to tmux if we're not in tmux

--- conf.d/000-tmux.user.fish
+++ conf.d/000-tmux.user.fish
@@ -4,18 +4,20 @@ end
 
 set fish_tmux_autostart false
 
-# Automatically connect to the session
-set sessions $(tmux ls -F "#{session_name}:#{session_attached}" 2> /dev/null)
-for s in $sessions
-    set parts $(string split ':' $s)
-    if [ $parts[1] = "default" ]
-        set default_session_state $parts[2]
-        break
+if not set -q TMUX
+    # Automatically connect to the session
+    set sessions $(tmux ls -F "#{session_name}:#{session_attached}" 2> /dev/null)
+    for s in $sessions
+        set parts $(string split ':' $s)
+        if [ $parts[1] = default ]
+            set default_session_state $parts[2]
+            break
+        end
     end
-end
 
-if not set -q default_session_state # Session does not exist
-    tmux new -s "default" > /dev/null
-else if [ $default_session_state = "0" ] # Session exists but is not attatched
-    tmux attach -t "default"
+    if not set -q default_session_state # Session does not exist
+        tmux new -s default >/dev/null
+    else if [ $default_session_state = 0 ] # Session exists but is not attached
+        tmux attach -t default
+    end
 end
